f31ffaa8c77265566cb09ff8fb16b828c49dc4e7
John Green <jpgreen@Johns-iMac.local>
2018-05-31 15:11:59 -0400

    Lesson 9

---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by John Green on -05-312018.
+//  Copyright © 2018 John P. Green. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
